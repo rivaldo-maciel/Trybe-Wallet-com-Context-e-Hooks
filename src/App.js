4bf8827e1f84ceb3e3dd286e9441133a68e2babd
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-01-22 22:42:24 -0300

    Adiciona Requisito-1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import UserProvider from "./context/UserProvider";
 
 function App() {
   return (
-    <main>
-      <h1>Trybe Wallet Com Context/Hooks</h1>
-    </main>
+    <UserProvider>
+      <main>
+        <h1>Trybe Wallet Com Context/Hooks</h1>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/wallet" component={() => {}} />
+        </Switch>
+      </main>
+    </UserProvider>
   );
 }
 
